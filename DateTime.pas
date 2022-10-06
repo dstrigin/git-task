41877dd1c920517635a67f902c9a0e684820e06d
Dmitriy Strigin <dmitriystrigin04@gmail.com>
2022-10-06 11:18:20 +0300

    Revert "Add convert from minutes to seconds"
    This reverts commit dde8d7260f3ed2fee690c78d98bb5a10d8ba7f6f.

--- DateTime.pas
+++ DateTime.pas
@@ -1,7 +1,11 @@
-﻿begin
+﻿##
+
+{1}
 var year := ReadInteger;
 Assert(year > 0);
 if ((year mod 4 = 0) and ((year mod 400 = 0) or (year mod 100 <> 0))) then
   println('True')
 else println('False');
-end.
+
+{2}
+println(60);
