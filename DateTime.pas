dde8d7260f3ed2fee690c78d98bb5a10d8ba7f6f
Dmitriy Strigin <dmitriystrigin04@gmail.com>
2022-10-06 11:18:00 +0300

    Add convert from minutes to seconds

--- DateTime.pas
+++ DateTime.pas
@@ -1,11 +1,7 @@
-﻿##
-
-{1}
+﻿begin
 var year := ReadInteger;
 Assert(year > 0);
 if ((year mod 4 = 0) and ((year mod 400 = 0) or (year mod 100 <> 0))) then
   println('True')
 else println('False');
-
-{2}
-println(60);
+end.
